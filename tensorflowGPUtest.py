3fb5b9f3480cf72666a0696c06efe9d47dba628b
612yese <240590367@qq.com>
2020-05-22 21:14:33 +0800

    添加了github测试代码

--- tensorflowGPUtest.py
+++ tensorflowGPUtest.py
@@ -23,63 +23,22 @@ import matplotlib.pyplot as plt
 
 print(tf.__version__)
 
-imdb = keras.datasets.imdb
+mnist = tf.keras.datasets.mnist
 
-(train_data, train_labels), (test_data, test_labels) = imdb.load_data(num_words=10000)
+(x_train, y_train), (x_test, y_test) = mnist.load_data()
+x_train, x_test = x_train / 255.0, x_test / 255.0
 
-# 一个映射单词到整数索引的词典
-word_index = imdb.get_word_index()
-
-# 保留第一个索引
-word_index = {k:(v+3) for k,v in word_index.items()}
-word_index["<PAD>"] = 0
-word_index["<START>"] = 1
-word_index["<UNK>"] = 2  # unknown
-word_index["<UNUSED>"] = 3
-
-reverse_word_index = dict([(value, key) for (key, value) in word_index.items()])
-
-def decode_review(text):
-    return ' '.join([reverse_word_index.get(i, '?') for i in text])
-
-train_data = keras.preprocessing.sequence.pad_sequences(train_data,
-                                                        value=word_index["<PAD>"],
-                                                        padding='post',
-                                                        maxlen=256)
-
-test_data = keras.preprocessing.sequence.pad_sequences(test_data,
-                                                       value=word_index["<PAD>"],
-                                                       padding='post',
-                                                       maxlen=256)
-
-# 输入形状是用于电影评论的词汇数目（10,000 词）
-vocab_size = 10000
-
-model = keras.Sequential()
-model.add(keras.layers.Embedding(vocab_size, 16))
-model.add(keras.layers.GlobalAveragePooling1D())
-model.add(keras.layers.Dense(16, activation='relu'))
-model.add(keras.layers.Dense(1, activation='sigmoid'))
-
-model.summary()
+model = tf.keras.models.Sequential([
+  tf.keras.layers.Flatten(input_shape=(28, 28)),
+  tf.keras.layers.Dense(128, activation='relu'),
+  tf.keras.layers.Dropout(0.2),
+  tf.keras.layers.Dense(10, activation='softmax')
+])
 
 model.compile(optimizer='adam',
-              loss='binary_crossentropy',
+              loss='sparse_categorical_crossentropy',
               metrics=['accuracy'])
 
-x_val = train_data[:10000]
-partial_x_train = train_data[10000:]
+model.fit(x_train, y_train, epochs=50)
 
-y_val = train_labels[:10000]
-partial_y_train = train_labels[10000:]
-
-history = model.fit(partial_x_train,
-                    partial_y_train,
-                    epochs=40,
-                    batch_size=512,
-                    validation_data=(x_val, y_val),
-                    verbose=1)
-
-results = model.evaluate(test_data,  test_labels, verbose=2)
-
-print(results)
+model.evaluate(x_test,  y_test, verbose=2)
