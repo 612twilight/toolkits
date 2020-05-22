7f94a987437ba5077bfa77eedd6773cc4f456fb6
612yese <240590367@qq.com>
2020-05-22 21:20:14 +0800

    添加了github测试代码

--- tensorflowGPUtest.py
+++ tensorflowGPUtest.py
@@ -23,16 +23,22 @@ import matplotlib.pyplot as plt
 
 print(tf.__version__)
 
-mnist = tf.keras.datasets.mnist
+def load_data(path="mnist.npz"):
+    with np.load(path, allow_pickle=True) as f:
+        x_train, y_train = f['x_train'], f['y_train']
+        x_test, y_test = f['x_test'], f['y_test']
 
-(x_train, y_train), (x_test, y_test) = mnist.load_data()
+        return (x_train, y_train), (x_test, y_test)
+
+
+(x_train, y_train), (x_test, y_test) = load_data()
 x_train, x_test = x_train / 255.0, x_test / 255.0
 
 model = tf.keras.models.Sequential([
-  tf.keras.layers.Flatten(input_shape=(28, 28)),
-  tf.keras.layers.Dense(128, activation='relu'),
-  tf.keras.layers.Dropout(0.2),
-  tf.keras.layers.Dense(10, activation='softmax')
+    tf.keras.layers.Flatten(input_shape=(28, 28)),
+    tf.keras.layers.Dense(128, activation='relu'),
+    tf.keras.layers.Dropout(0.2),
+    tf.keras.layers.Dense(10, activation='softmax')
 ])
 
 model.compile(optimizer='adam',
@@ -41,4 +47,4 @@ model.compile(optimizer='adam',
 
 model.fit(x_train, y_train, epochs=50)
 
-model.evaluate(x_test,  y_test, verbose=2)
+model.evaluate(x_test, y_test, verbose=2)
