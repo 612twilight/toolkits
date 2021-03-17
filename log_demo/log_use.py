15cbafc4a6eb58c7614753de88f49d33cd9ca5eb
twilight <240590367@qq.com>
2021-03-17 14:03:24 +0800

    logger

---
+++ log_demo/log_use.py
@@ -0,0 +1,14 @@
+# -*- coding:utf-8 -*-
+"""
+-------------------------------------------------
+Project Name: toolkits
+File Name: log_use.py
+Author: gaoyw
+Create Date: 2021/3/17
+-------------------------------------------------
+"""
+from log_demo.log1 import log
+
+log.info("info信息")
+log.debug("debug信息")
+log.error("error信息")
